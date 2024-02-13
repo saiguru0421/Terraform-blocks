804d984333f8a6b185cd8335a9fde64d9e6c1757
saiguru0421 <saiguru0421@gmail.com>
2024-02-13 14:55:18 -0500

    added documentation

--- EC2-instance.tf
+++ EC2-instance.tf
@@ -1,3 +1,4 @@
+#source block
 terraform {
     required_providers {
       aws={
@@ -6,11 +7,13 @@ terraform {
     }
 }
 
+#provider block
 provider "aws" {
   profile= "default"
   region= "ca-cental-1"
 }
 
+#resource block
 resource "aws_instance" "ec2" {
     ami = "ami-0b47e94e754413bcb"
     instance_type= "t2.micro"
